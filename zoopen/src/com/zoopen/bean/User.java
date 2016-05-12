7fb2c8919e605e3d5f22f576c2e04ff740847910
ytzyangting <ytzyangting@126.com>
2016-05-12 10:33:05 +0800

    嘴边拉邮编 (#11)
    * gtrgrtgtrgrt
    
    * Develop (#2) (#4)
    
    * 首次提交 (#1)
    
    * 添加 测试从分支合并到主分支
    
    * sxdcvb n
    
    * System.out.println("测试一下git好不好");
    
    * System.out.println("测试一下git好不好1");dfasdf
    
    * dsfsdfsdfs
    
    * sacdsacfasdfas
    
    * dasfdsfdsgsd
    
    * 犯得上犯得上发射点
    
    * gfdgdfg
    
    * 西安市擦
    
    * vdfvd
    
    * dsafbghn

--- zoopen/src/com/zoopen/bean/User.java
+++ zoopen/src/com/zoopen/bean/User.java
@@ -4,12 +4,15 @@ public class User {
 	private String name;
 
 	public String getName() {
+		System.out.println();
 		return name;
 	}
 
 	public void setName(String name) {
 		this.name = name;
+		System.out.println();
 	}
+
 	
 	
 }
